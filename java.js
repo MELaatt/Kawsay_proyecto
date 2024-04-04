e6e0e8cf0fe7c9a787e64e8b213bd71898df7043
MELaatt <125975585+MELaatt@users.noreply.github.com>
2024-04-03 23:00:28 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,54 @@
+const kawsay = document.getElementById("kawsay")
+
+
+
+window.addEventListener('scroll', function () {
+
+let miScroll = window.scrollY /8;
+
+    if(window.scrollY >= 300){
+        kawsay.style.transitionDuration=`1s`
+        kawsay.classList.remove (`opacity-0`);
+        kawsay.classList.remove(`mt-5`);
+    }
+
+    else {
+        kawsay.style.transitionDuration = '1s';
+        kawsay.classList.add('opacity-0');
+        kawsay.classList.add('mt-5');
+    }
+});
+
+
+const piedepaginaok = document.getElementById("piedepaginaok");
+
+window.addEventListener('scroll', function () {
+    let miScroll = window.scrollY / 8;
+
+    if (window.scrollY >= 2445.45458984375) {
+        piedepaginaok.style.transitionDuration = '1s';
+        piedepaginaok.classList.remove('opacity-0');
+        piedepaginaok.classList.remove('mt-5');
+    } else {
+        piedepaginaok.style.transitionDuration = '1s';
+        piedepaginaok.classList.add('opacity-0');
+        piedepaginaok.classList.add('mt-5');
+    }
+});
+
+const nuevoBtn = document.getElementById('mibtn')
+const videos = document.getElementById('videos')
+let estado = 1
+
+console.log(nuevoBtn.parentElement.firstElementChild.firstElementChild.firstElementChild.firstElementChild.id)
+
+function playVideo(videoId) {
+    console.log(nuevoBtn.parentElement.firstElementChild.firstElementChild.classList.contains('active'))
+    var video = document.getElementById(videoId);
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  }
+
